1171fac8461301fc0994549b575b9688a15a4561
tnapat2000 <tnapat2000@gmail.com>
2023-07-16 00:50:07 +0700

    add calling to dictionary api

--- src/main/java/com/example/linebot/Application.java
+++ src/main/java/com/example/linebot/Application.java
@@ -6,6 +6,13 @@ import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,6 +27,26 @@ public class Application {
     public Message handleTextMessage(MessageEvent<TextMessageContent> e) {
         System.out.println("event: " + e);
         TextMessageContent message = e.getMessage();
-        return new TextMessage(message.getText());
+        String urlString = "https://api.dictionaryapi.dev/api/v2/entries/en/" + message.getText().toString();
+        StringBuilder content= new StringBuilder();;
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+
+            in.close();
+            con.disconnect();
+
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
+        return new TextMessage(content.toString());
     }
 }
