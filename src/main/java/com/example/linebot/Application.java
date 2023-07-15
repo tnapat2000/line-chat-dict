c1e79a160256460e156c2ccaaeedc4ae709834b9
tnapat2000 <tnapat2000@gmail.com>
2023-07-16 00:04:15 +0700

    added LineMessageHandler in Application.java

--- src/main/java/com/example/linebot/Application.java
+++ src/main/java/com/example/linebot/Application.java
@@ -5,10 +5,12 @@ import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@LineMessageHandler
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
