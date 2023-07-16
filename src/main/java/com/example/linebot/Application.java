d1eadaadd5b83226d4b9b965b483983efa6eb382
tnapat2000 <tnapat2000@gmail.com>
2023-07-16 15:35:22 +0700

    finetuning outputs

--- src/main/java/com/example/linebot/Application.java
+++ src/main/java/com/example/linebot/Application.java
@@ -89,9 +89,9 @@ public class Application {
                             stringBuilder.append(" |> "+synonymArray.get(k) + "\n");
                         }
                     }
-                    if (j < meaningsJson.size()-1) {stringBuilder.append("\n");}
+                    stringBuilder.append("\n");
                 }
-                // if (i < jArray.size() -1) {stringBuilder.append("\n");}
+                if (i < jArray.size() -1) {stringBuilder.delete(stringBuilder.length()-2, stringBuilder.length()-1);}
             }
             if (stringBuilder.length() <= 0) {return new TextMessage(defaultResponseMessage);}
             return new TextMessage(stringBuilder.toString());
