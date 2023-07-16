b56714f65b30c915d397e2296fa4cf797fe6abd9
tnapat2000 <tnapat2000@gmail.com>
2023-07-16 14:40:39 +0700

    utilize JSONObject in formatting

--- src/main/java/com/example/linebot/Application.java
+++ src/main/java/com/example/linebot/Application.java
@@ -12,6 +12,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -22,6 +23,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @LineMessageHandler
 public class Application {
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
     }
 
@@ -52,16 +54,48 @@ public class Application {
             e1.printStackTrace();
         }
         
-        String responseMessage = "This word does not have meanings";
+        String defaultResponseMessage = "This word does not have meanings";
         JSONParser jParser = new JSONParser();
-        JSONObject jObject;
+        JSONArray jArray;
+        // return new TextMessage(content.toString());
+        StringBuilder stringBuilder = new StringBuilder();
         try {
-            jObject = (JSONObject) jParser.parse(content.toString());
-            return new TextMessage(content.toString());
+            jArray = (JSONArray) jParser.parse(content.toString());
+
+            for(int i = 0 ; i < jArray.size(); i++) {
+                JSONObject jObject= (JSONObject) jArray.get(i);                
+                JSONArray meaningsJson = (JSONArray) jObject.get("meanings");
+                // System.out.println(meaningsJson);
+                // System.out.println(meaningsJson.size());
+                for (int j = 0; j < meaningsJson.size(); j ++){
+                    // System.out.println(meaningsJson.get(j));
+                    JSONObject nounJsonObject = (JSONObject) meaningsJson.get(j);
+
+                    // part of speech
+                    String partOfSpeech = nounJsonObject.get("partOfSpeech").toString().substring(0, 1).toUpperCase();
+                    stringBuilder.append(partOfSpeech + ": ");
+
+                    // definitions
+                    JSONArray defJsonArray = (JSONArray) nounJsonObject.get("definitions");
+                    stringBuilder.append(((JSONObject)defJsonArray.get(0)).get("definition"));
+
+                    // synonyms
+                    JSONArray synonymArray = (JSONArray)nounJsonObject.get("synonyms");
+                    if (!synonymArray.isEmpty()) {
+                        stringBuilder.append("Synonyms: ");
+                        stringBuilder.append("/n");
+                        for (int k = 0; k < synonymArray.size(); k++){
+                            stringBuilder.append("|-"+synonymArray.get(k));
+                        }
+                    }
+                }
+            }
+            if (stringBuilder.isEmpty()) {return new TextMessage(defaultResponseMessage);}
+            return new TextMessage(stringBuilder.toString());
 
         } catch (ParseException e1) {
             e1.printStackTrace();
-            return new TextMessage("Error");
+            return new TextMessage(defaultResponseMessage);
         }
         // return new TextMessage(jObject.toString());
 
